67d7aacaeda8b5e0847b6cde4cc4b101947de34d
GameChanger2 <130241218+GameChanger2@users.noreply.github.com>
2025-09-09 18:55:13 +0200

    Update Zetarium.sol

--- Zetarium.sol
+++ Zetarium.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity 0.8.23;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Meet is ERC20 {
+    uint256 public immutable maxSupply;
+
+    constructor(
+        address owner
+        )
+        
+    ERC20("ZETARIUM", "ZET")
+    {
+        maxSupply = 1000000000 * 10 ** 18;
+
+        _mint(owner, maxSupply);
+    }
+}
